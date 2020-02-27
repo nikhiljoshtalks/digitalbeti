455293b98dbe22b511ca2542e1af82b5b79e7345
nikhilgupta18 <nikhilgupta1999@gmail.com>
2020-02-27 13:36:22 +0530

    update

--- digibeti/settings.py
+++ digibeti/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 from django.contrib.messages import constants as messages
 import django.conf.locale
 from django.conf import global_settings
@@ -156,7 +156,7 @@ LOGIN_URL = '/login'
 
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
